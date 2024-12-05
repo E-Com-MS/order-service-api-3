4709bef80ddc0dc36fe6cd97cc283bcd59a0bfe2
chinthaka senarathna <chironprabath@gmail.com>
2024-12-05 16:19:41 +0530

    in Authenticate ?

--- src/main/java/com/devops/order_service_api_3/api/TestController.java
+++ src/main/java/com/devops/order_service_api_3/api/TestController.java
@@ -1,5 +1,6 @@
 package com.devops.order_service_api_3.api;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order-service/api/v1/tests")
 public class TestController {
     @GetMapping()
+    @PreAuthorize("hasRole('user')")
     public String test(){
         return "Order Service Running on Port(8003)";
     }
